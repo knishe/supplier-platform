a4b5ddb248ede4d1b7772d987c494f7a043950ee
Nishanthan Krishnakumar <knishe@gmail.com>
2021-03-20 15:44:55 +0800

    [SUB-1] - Added database configuration

--- supplier-service/src/main/java/com/knishe/supplier/Supplier.java
+++ supplier-service/src/main/java/com/knishe/supplier/Supplier.java
@@ -1,4 +1,16 @@
 package com.knishe.supplier;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Data
+@Entity
+@NoArgsConstructor
 public class Supplier {
+    @Id
+    private Long id;
+    private String clientName;
 }
