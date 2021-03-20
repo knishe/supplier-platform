b75aa1b6e9c03dba4ee173e4e4a851315474c76a
Nishanthan Krishnakumar <knishe@gmail.com>
2021-03-20 15:49:46 +0800

    [SUB-1] - Added documentation

--- supplier-service/src/main/java/com/knishe/supplier/SupplierController.java
+++ supplier-service/src/main/java/com/knishe/supplier/SupplierController.java
@@ -1,5 +1,10 @@
 package com.knishe.supplier;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,8 +15,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/supplier")
 public class SupplierController {
 
+    @Operation(
+            summary = "",
+            description = ""
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",
+                    description = "Creates and returns an empty basket",
+                    content = @Content(schema = @Schema(implementation = SupplierResponse.class)))
+    })
     @GetMapping("{id}")
-    public ResponseEntity requestSupplier(@PathVariable String id){
+    public ResponseEntity requestSupplier(@PathVariable String id) {
         return ResponseEntity.ok().build();
+    }
+
+
+    class SupplierResponse {
     }
 }
