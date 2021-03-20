49d2641a8419b88e8f219cade9c4b2d7ed363b8f
Nishanthan Krishnakumar <knishe@gmail.com>
2021-03-20 16:01:11 +0800

    [SUB-1] - Removed unused imports

--- supplier-service/src/test/java/com/knishe/supplier/SupplierServiceTest.java
+++ supplier-service/src/test/java/com/knishe/supplier/SupplierServiceTest.java
@@ -3,12 +3,11 @@ package com.knishe.supplier;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class SupplierServiceTest {
 
     @Test
-    public void givenNoSupplierExist_whenCreateSupplier_thenReturnSupplier(){
+    void givenNoSupplierExist_whenCreateSupplier_thenReturnSupplier() {
         Supplier supplier = new Supplier();
         SupplierService service = new SupplierService();
         Supplier createdSupplier = service.createSupplier(supplier);
