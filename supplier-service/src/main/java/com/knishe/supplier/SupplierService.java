6e3dbd6df1f00b8bd80146ba6dbc5083a74e9a7a
Nishanthan Krishnakumar <knishe@gmail.com>
2021-03-23 22:48:16 +0800

    [SUP-2] - Added application event publisher

--- supplier-service/src/main/java/com/knishe/supplier/SupplierService.java
+++ supplier-service/src/main/java/com/knishe/supplier/SupplierService.java
@@ -1,11 +1,20 @@
 package com.knishe.supplier;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
 public class SupplierService {
+
+    private ApplicationEventPublisher applicationEventPublisher;
+
+    @Autowired
+    public SupplierService(ApplicationEventPublisher applicationEventPublisher) {
+        this.applicationEventPublisher = applicationEventPublisher;
+    }
 
     /**
      * Creates supplier
